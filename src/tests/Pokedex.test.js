2f31dc8e8a0957cab72262c36bce5625d79b54d4
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2023-01-23 17:17:20 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWhitRouter from '../renderWithRouter';
+import App from '../App';
+// import { Pokedex } from '../pages';
+
+describe('Teste o componente <Pokedex', () => {
+  it('Teste se pagina', () => {
+    renderWhitRouter(<App />);
+    const h2 = screen.getByRole('heading', { level: 2, name: /Encountered Pokémon/i });
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Test 2', () => {
+    renderWhitRouter(<App />);
+    userEvent.click(screen.getByText('Fire'));
+    expect(screen.getByText('Charmander')).toBeInTheDocument();
+    const type = screen.getAllByTestId('pokemon-type-button')[1];
+    userEvent.click(type);
+    expect(screen.getByTestId('pokemon-name')).toHaveTextContent('Charmander');
+
+    const proxPokemon = screen.getByRole('button', { name: /Próximo Pokémon/i });
+    userEvent.click(proxPokemon);
+    expect(screen.getByTestId('pokemon-name')).toHaveTextContent('Rapidash');
+    userEvent.click(screen.getByText(/all/i));
+    expect(screen.getByText('Pikachu')).toBeInTheDocument();
+  });
+});
